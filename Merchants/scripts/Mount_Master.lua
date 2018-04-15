dfbc7d5de6d07525b6d2ba07b32aba4d7822dbdb
Junior <junior.hard.soft@gmail.com>
2018-04-15 19:08:24 -0300

    Completed the implementation of the quest "Mount Master".

--- Merchants/scripts/Mount_Master.lua
+++ Merchants/scripts/Mount_Master.lua
@@ -11,15 +11,16 @@ function OnUse(npc, player, confirm)
 		return;
 	end
 	
-	local itemAttr = Items.GetItemById(mount:getId());
+	local itemAttr = iItems.GetItemById(mount:getId());
 	local price = itemAttr:getPrice();
+	local coin = player:getCoin();
 	
-	if (confirm == 0) then
+	if (coin == 0) then
 		iSend.Say(npc, "Para reviver ["..itemAttr:getName().."] sera necessario "..price.." de gold.");
 		return;
 	end
 	
-	if (player.Coin < price) then
+	if (coin < price) then
 		iSend.Say(npc, "Nao possui gold sufuciente!");
 		return;
 	end
@@ -29,15 +30,15 @@ function OnUse(npc, player, confirm)
 		return;
 	end
 
-	player.Coin = (player.Coin - price);
+	player:setCoin(coin - price);
 
 	local vitality = (iItems.GetItemAbility(mount, 79) - iRandomNumber(0, 3));
 
 	if (vitality > 0) then
 		iSend.Say(npc, "Ele foi Curado");
-		mount:setValue(0, 20);
-		mount.stEffect[1].cValue = vit;
-		mount.stEffect[2].cEffect = 5;
+		mount:setMountHp(20);
+		mount:setMountLife(vitality);
+		mount:setMountFood(5);
 	else
 		iSend.Say(npc, "Desculpe, nao pude salva-lo.");
 		mount:reset();
@@ -50,5 +51,5 @@ function OnUse(npc, player, confirm)
 	iSend.Score(player);
 	iSend.Etc(player);
 
-	iLog("etc, ["..player:getName().."] mount ressurect index:"..mount:getId().." vit:"..vitality.." - "..mount.stEffect[1].cEffect);
+	iLog("etc, ["..player:getName().."] mount ressurect index:"..mount:getId().." vit:"..vitality.." - "..mount:getMountLife());
 end

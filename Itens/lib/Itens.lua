e6586bd787afe553a34ed1d9b821358c5f1fb344
Junior <junior.hard.soft@gmail.com>
2018-05-22 00:38:57 -0300

    Added utility function createItem for create CItem type. Added utility function getClassIdByCorpseId for return the player class id by corpse id. Added more Npcs. Minor changes for Npcs and itemlist.xml.

--- Itens/lib/Itens.lua
+++ Itens/lib/Itens.lua
@@ -12,3 +12,46 @@ function sendToWater(player, event, level)
 	
 	return FALSE;
 end
+
+function createItem(itemId, effct1, effct2, effct3)
+	local item = CItem:new(itemId);
+	
+	if(effct1 ~= nil) then
+		item:setAddValue(0, effct1[1], effct1[2]);
+	end
+	
+	if(effct2 ~= nil) then
+		item:setAddValue(1, effct2[1], effct2[2]);
+	end
+	
+	if(effct3 ~= nil) then
+		item:setAddValue(2, effct3[1], effct3[2]);
+	end
+	
+	return item;
+end
+
+function getClassIdByCorpseId(player)
+
+	local classId = player:getClass();
+	if(classId >= 0 and classId <= 3) then
+		
+		local corpseId = player:getPhace():getId();
+		if( corpseId >= 1 and corpseId <= 9 ) then
+			return 1;
+		elseif( corpseId >= 11 and corpseId <= 19 ) then
+			return 2;
+		elseif( corpseId >= 21 and corpseId <= 29 ) then
+			return 3;
+		elseif( corpseId >= 31 and corpseId <= 39 ) then
+			return 4;
+		else
+			return -1;
+		end
+		
+	else
+		return -1;
+	end
+	
+	return -1;
+end

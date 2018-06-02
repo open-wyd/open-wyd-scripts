27f9c99529febba494cef9918e5700a68dd3d626
Junior <junior.hard.soft@gmail.com>
2018-06-02 16:34:57 -0300

    Added config.lua

---
+++ Data/config.lua
@@ -0,0 +1,45 @@
+-- server name
+servername = "Open_Wyd_Server"
+
+-- server location
+location = "Brazil"
+
+-- server url
+url = "http://openwyd.com.br"
+
+-- server owner name
+ownername = "Junior"
+
+-- server owner email
+owneremail = "suport@openwyd.com.br"
+
+-- world name
+worldname = "OWS"
+
+-- world type
+-- options: pvp, no-pvp, pvp-enforced
+worldtype = "pvp"
+
+-- exhausted time in ms (1000 = 1 second) for none-aggressive spells/weapons
+exhausted = 1000
+
+-- exhausted time in ms (1000 = 1 second) for aggressive spells/weapons
+fightexhausted = 2000
+
+-- how many ms to add if the player is already exhausted and tries to cast a spell (1000 = 1 second)
+exhaustedadd = 200
+
+-- Extra experience % per player when sharing experience
+party_exp_mul = 5
+
+-- rates (experience, skill, magic level, loot and spawn)
+rate_exp_free = 20;
+rate_loot_free = 8;
+rate_gold_free = 1;
+
+-- login message
+loginmsg = "Welcome to Open WYD. For help visit http://openwyd.com.br"
+
+-- max number of players allowed
+maxplayers = "1000"
+
